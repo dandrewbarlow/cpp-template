a67dd64f613164b04727f81e39fe96f421d1b8af
Andrew Barlow <dandrewbarlow@gmail.com>
2020-02-28 22:02:00 -0700

    add template

---
+++ c++.cpp
@@ -0,0 +1,49 @@
+// Andrew Barlow
+// Description:
+//
+// v 0.00
+
+// LIBRARIES//////////////////////////////////////////////
+
+// Input / Output Streams
+#include <iostream>
+// File Streams
+#include <fstream>
+// String Streams
+#include <sstream>
+// Strings
+#include <string>
+// Vectors
+#include <vector>
+// Random Number Generators
+#include <random>
+// Time
+#include <time.h>
+// curses: advanced console functionality
+#include <curses.h>
+
+// CLASSES////////////////////////////////////////////////
+
+// HELPER FUNCTIONS///////////////////////////////////////
+void ui(int input) {
+  switch (input) {
+    case /* value */:
+      // insert code
+      break;
+  }
+  return;
+}
+
+// INITIALIZE/////////////////////////////////////////////
+
+void init() {
+  //insert any initilization here
+  // srand(time(NULL));
+}
+
+// MAIN //////////////////////////////////////////////////
+int int main(int argc, char const *argv[]) {
+
+
+  return 0;
+}
