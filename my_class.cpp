2d7efaad10227ad76b1bb9bc36940099d7893e88
Andrew Barlow <dandrewbarlow@gmail.com>
2020-12-25 20:45:27 -0600

    template class implementation

---
+++ my_class.cpp
@@ -0,0 +1,4 @@
+#include "my-class.h"
+
+// define class instances
+// my_class::Func() {return;}
---
+++ my_class.h
@@ -0,0 +1,8 @@
+class my_class
+{
+private:
+	// private variables
+public:
+	// public variables/methods
+
+}
